1fdee785514edc8c335bd9d4fb891e1a3af31b39
Dasha Bayanova <d.bayanova@dodopizza.com>
2018-03-26 15:43:26 +0300

    Remove comments, rename local variables

--- VideoStore/Customer.cs
+++ VideoStore/Customer.cs
@@ -6,43 +6,32 @@ namespace VideoStore
 {
 	public class Customer 
 	{
-		private readonly string _name;
-		private readonly List<Rental> _rentals = new List<Rental>();
+		private readonly List<Rental> rentals = new List<Rental>();
 
 		public Customer(string name) 
 		{
-			this._name = name;
+			this.Name = name;
 		}
 
 		public void AddRental(Rental arg) 
 		{
-			_rentals.Add(arg);
+			rentals.Add(arg);
 		}
 
-		public string Name
-		{
-			get
-			{
-				return _name;
-			}
-		}
+		public string Name { get; }
 
 		public string Statement() 
 		{
 			double totalAmount = 0;
 			int frequentRenterPoints = 0;
 
-			// Select every inside of the parantheses and extract to method called CreateHeader
 			string result = "Rental Record for " + Name + "\n";
 
-			foreach (Rental each in _rentals)
+			foreach (Rental each in rentals)
 			{
-				// Extract Method 
-				// Highlight all of the text below to the end of the switch statment
 				double thisAmount = 0;
 
-				//determine amounts for each line
-				switch (each.Movie.PriceCode) 
+				switch (each.Movie.PriceCode)
 				{
 					case Movie.REGULAR:
 						thisAmount += 2;
@@ -69,30 +58,21 @@ namespace VideoStore
 
 						break;
 				}
-				// End of switch statement
-				// If you're using Resharper 2.0 keymap, press Ctrl-Alt-M
-				// If you're using Visual Studio keymap, press Ctrl+R,M
-
-				// add frequent renter points
 				frequentRenterPoints++;
 
-				// add bonus for a two day new release rental
 				if ((each.Movie.PriceCode == Movie.NEW_RELEASE)
 					&& (each.DaysRented > 1)) 
 				{
 					frequentRenterPoints++;
 				}
 
-				//show figures for this rental
-				// Select every inside of the parantheses and extract to method called CreateLine
-				result += ("\t" + each.Movie.Title + "\t" + thisAmount.ToString() + "\n");
+				result += ("\t" + each.Movie.Title + "\t" + thisAmount + "\n");
 				totalAmount += thisAmount;
 			}
 
 
-			//add footer lines
-			result += ("Amount owed is " + totalAmount.ToString() + "\n");
-			result += ("You earned " + frequentRenterPoints.ToString() + " frequent renter points");
+			result += ("Amount owed is " + totalAmount + "\n");
+			result += ("You earned " + frequentRenterPoints + " frequent renter points");
 
 			return result;
 		}
