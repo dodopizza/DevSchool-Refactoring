44c1f12bb85ef582101fc0fd3217cf68e27a75b5
Sergey Zarubin <s.zarubin@dodopizza.com>
2018-03-26 15:46:12 +0300

    Extracted GetAmount from Customer.Statement

--- VideoStore/Customer.cs
+++ VideoStore/Customer.cs
@@ -37,64 +37,59 @@ namespace VideoStore
 
 			foreach (Rental each in _rentals)
 			{
-				// Extract Method 
-				// Highlight all of the text below to the end of the switch statment
-				double thisAmount = 0;
-
-				//determine amounts for each line
-				switch (each.Movie.PriceCode) 
-				{
-					case Movie.REGULAR:
-						thisAmount += 2;
-
-						if (each.DaysRented > 2) 
-						{
-							thisAmount += ((each.DaysRented - 2) * 1.5);
-						}
-
-						break;
-
-					case Movie.NEW_RELEASE:
-						thisAmount += (each.DaysRented * 3);
-
-						break;
-
-					case Movie.CHILDRENS:
-						thisAmount += 1.5;
-
-						if (each.DaysRented > 3) 
-						{
-							thisAmount += ((each.DaysRented - 3) * 1.5);
-						}
-
-						break;
-				}
-				// End of switch statement
-				// If you're using Resharper 2.0 keymap, press Ctrl-Alt-M
-				// If you're using Visual Studio keymap, press Ctrl+R,M
-
-				// add frequent renter points
+				double thisAmount = GetRentalCost(each);
 				frequentRenterPoints++;
 
-				// add bonus for a two day new release rental
 				if ((each.Movie.PriceCode == Movie.NEW_RELEASE)
 					&& (each.DaysRented > 1)) 
 				{
 					frequentRenterPoints++;
 				}
 
-				//show figures for this rental
-				// Select every inside of the parantheses and extract to method called CreateLine
 				result += ("\t" + each.Movie.Title + "\t" + thisAmount.ToString() + "\n");
 				totalAmount += thisAmount;
 			}
 
 
-			//add footer lines
 			result += ("Amount owed is " + totalAmount.ToString() + "\n");
 			result += ("You earned " + frequentRenterPoints.ToString() + " frequent renter points");
 
 			return result;
+		}
+
+		private double GetRentalCost(Rental rental)
+		{
+			var thisAmount = 0d;
+			
+			switch (rental.Movie.PriceCode) 
+			{
+				case Movie.REGULAR:
+					thisAmount += 2;
+
+					if (rental.DaysRented > 2) 
+					{
+						thisAmount += ((rental.DaysRented - 2) * 1.5);
+					}
+
+					break;
+
+				case Movie.NEW_RELEASE:
+					thisAmount += (rental.DaysRented * 3);
+
+					break;
+
+				case Movie.CHILDRENS:
+					thisAmount += 1.5;
+
+					if (rental.DaysRented > 3) 
+					{
+						thisAmount += ((rental.DaysRented - 3) * 1.5);
+					}
+
+					break;
+			}
+
+			return thisAmount;
 		}
 	}
 }
