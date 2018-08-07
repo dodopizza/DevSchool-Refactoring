d18c24fc44a2d3a7b56dd0a145fb2cd9468f1b38
Nikita Starichenko <n.starichenko@dodopizza.com>
2018-08-07 15:56:32 +0300

    Refactor

--- VideoStore/Customer.cs
+++ VideoStore/Customer.cs
@@ -27,9 +27,9 @@ namespace VideoStore
 
 			foreach (var rental in _rentals)
 			{
-				var thisAmount = GetCostFor(rental);
+				var thisAmount = rental.GetCost();
 
-				frequentRenterPoints += FrequentRenterPointsFor(rental);
+				frequentRenterPoints += rental.GetFrequentRenterPoints();
 
 				result += ("\t" + rental.Movie.Title + "\t" + thisAmount + "\n");
 				totalAmount += thisAmount;
@@ -39,50 +39,6 @@ namespace VideoStore
 			result += ("You earned " + frequentRenterPoints + " frequent renter points");
 
 			return result;
-		}
-
-		private static int FrequentRenterPointsFor(Rental rental)
-		{
-			var frequentRenterPoints = 1;
-
-			if (rental.Movie.PriceCode == Movie.NEW_RELEASE && rental.DaysRented > 1)
-			{
-				frequentRenterPoints++;
-			}
-
-			return frequentRenterPoints;
-		}
-
-		private static double GetCostFor(Rental rental)
-		{
-			double thisAmount = 0;
-
-			switch (rental.Movie.PriceCode)
-			{
-				case Movie.REGULAR:
-					thisAmount += 2;
-
-					if (rental.DaysRented > 2)
-					{
-						thisAmount += ((rental.DaysRented - 2) * 1.5);
-					}
-
-					break;
-				case Movie.NEW_RELEASE:
-					thisAmount += (rental.DaysRented * 3);
-					break;
-				case Movie.CHILDRENS:
-					thisAmount += 1.5;
-
-					if (rental.DaysRented > 3)
-					{
-						thisAmount += ((rental.DaysRented - 3) * 1.5);
-					}
-
-					break;
-			}
-
-			return thisAmount;
 		}
 	}
 }
